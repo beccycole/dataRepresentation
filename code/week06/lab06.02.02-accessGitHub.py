8851639989aae447518295b25ecad2db73a181f3
Rebecca Cole <g00364727@gmit.ie>
2019-11-09 19:24:53 +0000

    Video 2 week 6

---
+++ code/week06/lab06.02.02-accessGitHub.py
@@ -0,0 +1,17 @@
+
+import requests
+import json
+
+
+# remove the minus sign
+apiKey = 'b55d312da577ba479f7dc4f8f3f5b1384bdf3b2-e'
+url = 'https://api.github.com/repos/datarepresentationstudent/aPrivateOne'
+filename ="repo.json"
+
+response = requests.get(url, auth=('token',apiKey))
+
+repoJSON = response.json()
+#print (response.json())
+
+file = open(filename, 'w')
+json.dump(repoJSON, file, indent=4)
