8c290f1946a45436a32c683f995fb2c71db147e4
Rebecca Cole <g00364727@gmit.ie>
2019-12-20 00:05:44 +0000

    Resolved server issue and created html file

--- code/project/testConvert.py
+++
@@ -1,9 +0,0 @@
-colnames=['id','title','year','budget','director']
-result=(1,"Toy Story", 1995, 60000000, "John Lasseter")
-item = {}
-
-for i, colName in enumerate(colnames):
-    value = result[i]
-    item[colName] = value
-
-print(item)
