06a80bb149506d7d87faa5e18d9aa109c339c4e9
Rebecca Cole <g00364727@gmit.ie>
2019-12-20 21:25:15 +0000

    Started project again as bug in code

--- code/project/server.py
+++ code/project/server.py
@@ -1,41 +1,39 @@
 from flask import Flask, jsonify, request, abort
-from filmDAO import filmDAO
 
 app = Flask(__name__, static_url_path='', static_folder='.')
 
-films = [{ "id":1, "Title":"Toy Story", "Year":1995, "Budget":30000000, "Director":"John Lasseter"},
-    { "id":2, "Title":"Heat", "Year":1995, "Budget":60000000, "Director":"Michael Mann"},
-    { "id":3, "Title":"Casino", "Year":1995, "Budget":52000000, "Director":"Martin Scorsese"}
+films=[
+    { "id":1, "Title":"Toy Story", "Year":1995, "Budget":50000000, "Director":"John Lasseter"},
+    { "id":2, "Title":"Casino", "Year":1995, "Budget":60000000, "Director":"Martin Scorsese"},
+    { "id":3, "Title":"The Hangover", "Year":2010, "Budget":50000000, "Director":"Todd Phillips"}
 ]
 nextId=4
-
 
 #app = Flask(__name__)
 
 #@app.route('/')
 #def index():
-  #return "Hello, World!"
+  #  return "Hello, World!"
 
-#curl "http://127.0.0.1:5000/films"
 @app.route('/films')
 def getAll():
-    results = filmDAO.getAll()
-    return jsonify(results)
+    return jsonify(films)
 
-#curl "http://127.0.0.1:5000/films/2"
+#curl http://127.0.0.1:5000/films/2
 @app.route('/films/<int:id>')
 def findById(id):
-    foundFilm = filmDAO.findById(id)
+    foundFilms = list(filter(lambda t: t['id']==id, films))
+    if len(foundFilms) == 0:
+        return jsonify ({}) , 204
+    return jsonify(foundFilms[0])
 
-    return jsonify(foundFilm)
-
-#curl  -i -H "Content-Type:application/json" -X POST -d "{\"Title\":\"Elf\",\"Year\":2002,\"Budget\":11000000,\"Director\":\"Jon Favreau\"}" http://127.0.0.1:5000/films
+#curl  -i -H "Content-Type:application/json" -X POST -d "{\"Title\":\"Due Date\",\"Year\":2001,\"Budget\":60000000,\"Director\":\"Todd Phillips\"}" http://127.0.0.1:5000/films
 @app.route('/films', methods=['POST'])
 def create():
     global nextId
     if not request.json:
         abort(400)
-    # other checking 
+    
     film = {
         "id": nextId,
         "Title": request.json['Title'],
@@ -45,9 +43,9 @@ def create():
     }
     nextId += 1
     films.append(film)
-    return jsonify(films)
+    return jsonify(film)
 
-#curl  -i -H "Content-Type:application/json" -X PUT -d "{\"Title\":\"The Family\",\"Year\":2013,\"Budget\":30000000,\"Director\":\"Jesse Moss\"}" http://127.0.0.1:5000/films/1
+#curl  -i -H "Content-Type:application/json" -X PUT -d "{\"Budget\":70000000}" http://127.0.0.1:5000/films/1
 @app.route('/films/<int:id>', methods=['PUT'])
 def update(id):
     foundFilms = list(filter(lambda t: t['id']== id, films))
@@ -57,9 +55,9 @@ def update(id):
     if not request.json:
         abort(400)
     reqJson = request.json
-    if 'Budget' in reqJson and type(reqJson['Budget']) is not int:
-        abort(400)
     if 'Year' in reqJson and type(reqJson['Year']) is not int:
+        abort(400)
+    if 'Budget' in reqJson and type(reqJson['Budget']) is not int:
         abort(400)
 
     if 'Title' in reqJson:
@@ -70,22 +68,18 @@ def update(id):
         foundFilm['Budget'] = reqJson['Budget']
     if 'Director' in reqJson:
         foundFilm['Director'] = reqJson['Director']
- 
- 
+
     return jsonify(foundFilm)
-        
 
-    return "in update for id "+str(id)
-
-
-#curl -X DELETE "http://127.0.0.1:5000/films/1"
-@app.route('/films/<int:id>' , methods=['DELETE'])
+#curl -X DELETE http://127.0.0.1:5000/films/5
+@app.route('/films/<int:id>', methods=['DELETE'])
 def delete(id):
     foundFilms = list(filter(lambda t: t['id']== id, films))
     if (len(foundFilms) == 0):
         abort(404)
     films.remove(foundFilms[0])
     return jsonify({"done":True})
+
 
 
 if __name__ == '__main__' :
